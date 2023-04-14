d426f65480500bf7a6955b7e383c27a55ccc6623
dani morillo <danifromecuador@gmail.com>
2023-04-14 02:22:57 -0500

    Create refresh function

--- src/modules/sendAndReceive.js
+++ src/modules/sendAndReceive.js
@@ -1,4 +1,27 @@
+export const refresh = async () => {
+  try {
+    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/SNypXgDAYR2s53JoVMhD/scores');
+    const data = await response.json();
+    console.log(data.result[0].user);
+    // console.log(dataArray[0].user);
 
+    let justDoIt = `result: [{score: 1, user: "a"}, {score: 1, user: "a"}, {score: 123, user: "ba"}, {score: 213, user: "a"}]`;
+
+    const scoreContainer = document.querySelector('.score-container');
+
+    // let html = `
+    //   <li>${data[0].user}</li>
+    // `;
+    // scoreContainer.innerHTML = html;
+
+
+
+
+
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const addYourData = () => {
   const inputName = document.querySelector('.inputName');
