369c9e10cb731a662030fdbd7d4273ed98441094
dani morillo <danifromecuador@gmail.com>
2023-04-14 14:44:59 -0500

    Create getData function that returns a JSON object

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import _ from 'lodash';
 // import './style.css';
-import { refresh, addYourData, createNewGame } from './modules/sendAndReceive.js';
+import { refresh } from './modules/sendAndReceive.js';
 
 const refreshBtn = document.querySelector('.refreshBtn');
 refreshBtn.addEventListener('click', () => {
@@ -12,4 +12,4 @@ submitBtn.addEventListener('click', () => {
   addYourData();
 });
 
-window.onload = createNewGame();
+// window.onload = createNewGame();
--- src/modules/sendAndReceive.js
+++ src/modules/sendAndReceive.js
@@ -1,66 +1,63 @@
-export const refresh = async () => {
+// export const refresh = async () => {
+//   try {
+//     const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/SNypXgDAYR2s53JoVMhD/scores');
+//     const data = await response.json();
+
+//     const scoreContainer = document.querySelector('.score-container');
+//     scoreContainer.innerHTML = '';
+//     for (let i = 0; i < data.result.length; i += 1) {
+//       const html = `<li>${data.result[i].user}: ${data.result[i].score}</li>`;
+//       scoreContainer.innerHTML += html;
+//     }
+//   } catch (error) {
+//     const errorMessage = document.createElement('div');
+//     errorMessage.textContent = 'An error occurred. Please try again later.';
+//     document.body.appendChild(errorMessage);
+//   }
+// };
+
+// export const addYourData = () => {
+//   const inputName = document.querySelector('.inputName');
+//   const inputScore = document.querySelector('.inputScore');
+//   if (inputName !== '' && inputScore !== '') {
+//     const dataToSend = {
+//       user: inputName.value,
+//       score: parseInt(inputScore.value, 10),
+//     };
+
+//     fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/SNypXgDAYR2s53JoVMhD/scores', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(dataToSend),
+//     })
+//       .then((response) => response.json());
+//     // .then((data) => console.log(data))
+//     // .catch((error) => console.error(error));
+//   }
+//   inputName.value = '';
+//   inputScore.value = '';
+// };
+
+export const getData = async () => {
   try {
-    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/SNypXgDAYR2s53JoVMhD/scores');
-    const data = await response.json();
-    console.log(data.result[0].user);
-    // console.log(dataArray[0].user);
-
-    let justDoIt = `result: [{score: 1, user: "a"}, {score: 1, user: "a"}, {score: 123, user: "ba"}, {score: 213, user: "a"}]`;
-
-    const scoreContainer = document.querySelector('.score-container');
-
-    // let html = `
-    //   <li>${data[0].user}</li>
-    // `;
-    // scoreContainer.innerHTML = html;
-
-
-
-
-
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-export const addYourData = () => {
-  const inputName = document.querySelector('.inputName');
-  const inputScore = document.querySelector('.inputScore');
-  if (inputName !== '' && inputScore !== '') {
-    let dataToSend = {      
-      "user": inputName.value,
-      "score": parseInt(inputScore.value)
-    };
-
-    fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/SNypXgDAYR2s53JoVMhD/scores', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(dataToSend)
-    })
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error(error));
-  }
-  inputName.value = "";
-  inputScore.value = "";
-};
-
-export const createNewGame = async () => {
-  try {
-    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        name: "my new gamez"
-      })
+    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/SNypXgDAYR2s53JoVMhD/scores', {
+      method: 'GET',
     });
-    const data = await response.json();
-    console.log(data);
+    const responseData = await response.json();
+    return responseData;
   } catch (error) {
-    console.error(error);
+    return error;
   }
 };
+
+// export const refresh = async () => {
+//   const data = await getData();
+//   const scoreContainer = document.querySelector('.score-container');
+//   scoreContainer.innerHTML = '';
+//   for (let i = 0; i < data.result.length; i += 1) {
+//     const html = `<li>${data.result[i].user}: ${data.result[i].score}</li>`;
+//     scoreContainer.innerHTML += html;
+//   }
+// };
